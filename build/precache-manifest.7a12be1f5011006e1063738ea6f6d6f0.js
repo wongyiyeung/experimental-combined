58a5bd9dc54fc5e3514ea2fce2846164e4f7fb5b
yiyeung wong <wongyiyeung@gmail.com>
2020-08-31 20:34:34 +0800

    commit combined version of notes app

---
+++ build/precache-manifest.7a12be1f5011006e1063738ea6f6d6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22f3dbea9ddefc1efe09fd97ff14e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495822fd6b2474a199b4",
+    "url": "/static/css/main.b2520b0c.chunk.css"
+  },
+  {
+    "revision": "a3b661d7b6e26262927e",
+    "url": "/static/js/2.020ea15f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.020ea15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495822fd6b2474a199b4",
+    "url": "/static/js/main.a0e692d5.chunk.js"
+  },
+  {
+    "revision": "fc927c064f2646f05b4d",
+    "url": "/static/js/runtime-main.11ea6cb5.js"
+  }
+]);
---
+++ build/precache-manifest.8625267c071ec2b6f560708468bd5204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990313262b462e222eb2798ea8cb6bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94711322f5fb65223e48",
+    "url": "/static/css/main.b2520b0c.chunk.css"
+  },
+  {
+    "revision": "a3b661d7b6e26262927e",
+    "url": "/static/js/2.020ea15f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.020ea15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94711322f5fb65223e48",
+    "url": "/static/js/main.c3f9c06f.chunk.js"
+  },
+  {
+    "revision": "fc927c064f2646f05b4d",
+    "url": "/static/js/runtime-main.11ea6cb5.js"
+  }
+]);
